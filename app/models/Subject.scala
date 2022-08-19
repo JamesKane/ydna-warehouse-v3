31c7308343b9af1cc8b8ee2c01e3378eb7868ddc
jkane <jkane@tds.net>
2022-08-19 12:04:58 -0500

    Variant and Subject model should be complete

--- app/models/Subject.scala
+++ app/models/Subject.scala
@@ -1,16 +1,10 @@
 package models
 
-import models.EventType.EventType
 import org.joda.time.DateTime
 import play.api.libs.json._
 import reactivemongo.api.bson._
-
-object EventType extends Enumeration {
-  type EventType = Value
-
-  val BIRTH = Value(1)
-  val DEATH = Value(2)
-}
+import reactivemongo.play.json.compat._
+import bson2json._
 
 /**
  * LifeEvent
@@ -32,15 +26,17 @@ case class LifeEvent(
  *
  * Information about the most distant known ancestor
  *
- * @param givenName       The Given Name
+ * @param givenName   The Given Name
  * @param surname     The Family Name
- * @param lifeEvents  Map of Life Events
+ * @param birthInfo   Birth event
+ * @param deathInfo   Death event
  * @param info        Free-form additional details
  */
 case class MdkaInfo(
                      givenName: Option[String] = None,
                      surname: Option[String] = None,
-                     lifeEvents: Map[EventType, LifeEvent] = Map(),
+                     birthInfo: Option[LifeEvent] = None,
+                     deathInfo: Option[LifeEvent] = None,
                      info: Option[String] = None
                    )
 
@@ -72,30 +68,99 @@ case class Subject(
 
 object LifeEvent {
   implicit object LifeEventWrites extends OWrites[LifeEvent] {
-    override def writes(o: LifeEvent): JsObject = ???
+    override def writes(o: LifeEvent): JsObject = Json.obj(
+      "date" -> o.date,
+      "place" -> o.place,
+      "point" -> o.point
+    )
   }
 
   implicit object LifeEventReads extends Reads[LifeEvent] {
-    override def reads(json: JsValue): JsResult[LifeEvent] = ???
+    override def reads(json: JsValue): JsResult[LifeEvent] = json match {
+      case obj: JsObject => try {
+        JsSuccess(
+          LifeEvent(
+            (obj \ "date").asOpt[String],
+            (obj \ "place").asOpt[String],
+            (obj \ "point").asOpt[GeoPoint]
+          )
+        )
+      } catch {
+        case cause: Throwable => JsError(cause.getMessage)
+      }
+
+      case _ => JsError("expected.jsobject")
+    }
   }
 }
 
 object MdkaInfo {
   implicit object MdkaInfoWrites extends OWrites[MdkaInfo] {
-    override def writes(o: MdkaInfo): JsObject = ???
+    override def writes(o: MdkaInfo): JsObject = Json.obj(
+      "givenName" -> o.givenName,
+      "surname" -> o.surname,
+      "birthInfo" -> o.birthInfo,
+      "deathInfo" -> o.deathInfo,
+      "info" -> o.info
+    )
   }
 
   implicit object MdkaInfoReads extends Reads[MdkaInfo] {
-    override def reads(json: JsValue): JsResult[MdkaInfo] = ???
+    override def reads(json: JsValue): JsResult[MdkaInfo] = json match {
+      case obj: JsObject => try {
+        val maybeInfo = (obj \ "info").asOpt[String]
+        JsSuccess(
+          MdkaInfo(
+            givenName = (obj \ "givenName").asOpt[String],
+            surname = (obj \ "surname").asOpt[String],
+            birthInfo = (obj \ "birthInfo").asOpt[LifeEvent],
+            deathInfo = (obj \ "deathInfo").asOpt[LifeEvent],
+            info = maybeInfo
+          )
+        )
+      } catch {
+        case cause: Throwable => JsError(cause.getMessage)
+      }
+      case _ => JsError("expected.jsobject")
+    }
   }
 }
 
 object Subject {
   implicit object SubjectWrites extends OWrites[Subject] {
-    override def writes(o: Subject): JsObject = ???
+    override def writes(o: Subject): JsObject = Json.obj(
+      "_id" -> o._id,
+      "_creationDate" -> o._creationDate.fold(-1L)(_.getMillis),
+      "_updateDate" -> o._updateDate.fold(-1L)(_.getMillis),
+      "displayName" -> o.displayName,
+      "taxaID" -> o.taxaID,
+      "paternalMdka" -> o.paternalMdka,
+      "ySubclade" -> o.ySubclade,
+      "maternalMdka" -> o.maternalMdka,
+      "mtSubclade" -> o.mtSubclade
+    )
   }
 
   implicit object SubjectReads extends Reads[Subject] {
-    override def reads(json: JsValue): JsResult[Subject] = ???
+    override def reads(json: JsValue): JsResult[Subject] = json match {
+      case obj: JsObject => try {
+        JsSuccess(
+          Subject(
+            (obj \ "_id").asOpt[BSONObjectID],
+            (obj \ "_creationDate").asOpt[Long].map(new DateTime(_)),
+            (obj \ "_updateDate").asOpt[Long].map(new DateTime(_)),
+            (obj \ "displayName").as[String],
+            (obj \ "taxaID").as[String],
+            (obj \ "paternalMdka").asOpt[MdkaInfo],
+            (obj \ "ySubclade").asOpt[BSONObjectID],
+            (obj \ "maternalMdka").asOpt[MdkaInfo],
+            (obj \ "mtSubclade").asOpt[BSONObjectID]
+          )
+        )
+      } catch {
+        case cause: Throwable => JsError(cause.getMessage)
+      }
+      case _ => JsError("expected.jsobject")
+    }
   }
 }
