0f1fd6f9712490094f536a235f091088d841ddbc
jkane <jkane@tds.net>
2022-08-19 20:02:47 -0500

    Refactoring the common error returns to a singleton

--- app/controllers/VariantController.scala
+++ app/controllers/VariantController.scala
@@ -16,10 +16,9 @@ import scala.util.{Failure, Success}
  * @author James R. Kane
  * @version 3.0
  * @since 2022-08-19
- *
- * @param ec                    An implicit execution context
- * @param repo                  A variant repository
- * @param controllerComponents  Play controller components
+ * @param ec                   An implicit execution context
+ * @param repo                 A variant repository
+ * @param controllerComponents Play controller components
  */
 class VariantController @Inject()(implicit val ec: ExecutionContext, val repo: VariantRepository, val controllerComponents: ControllerComponents) extends BaseController {
 
@@ -35,25 +34,23 @@ class VariantController @Inject()(implicit val ec: ExecutionContext, val repo: V
         }
       case Failure(_) => ControllerHelper.VariantIdParseFailure
     }
-
   }
 
   def create(): Action[JsValue] = Action.async(controllerComponents.parsers.json) { implicit request =>
     request.body.validate[Variant].fold(
-      _ => Future.successful(BadRequest("Cannot parse request body")),
+      _ => ControllerHelper.FutureBodyParseFailure,
       variant => repo.create(variant).map { _ => Created(Json.toJson(variant)) }
     )
   }
 
   def update(id: String): Action[JsValue] = Action.async(controllerComponents.parsers.json) { implicit request =>
     request.body.validate[Variant].fold(
-      _ => Future.successful(BadRequest("Cannot parse request body")),
+      _ => ControllerHelper.FutureBodyParseFailure,
       variant =>
         BSONObjectID.parse(id) match {
           case Success(value) => repo.update(value, variant).map { result => Ok(Json.toJson(result.code)) }
           case Failure(_) => ControllerHelper.VariantIdParseFailure
         }
-
     )
   }
 
